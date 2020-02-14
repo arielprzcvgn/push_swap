380ef2c07ec5f22635184f86fde981c53b45f088
Ariel PEREZ <ariperez@e2r7p3.42.fr>
2020-02-14 15:12:34 +0100

    Adding color option

--- srcs/visu.c
+++ srcs/visu.c
@@ -12,14 +12,21 @@
 
 #include "../includes/push_swap.h"
 
-void	print_pile(t_list **pile, int len)
+void	print_pile(t_list **pile, t_opt *o, t_list *old)
 {
 	t_list	*current;
+	int		len;
 
+	len = o->len;
 	current = *pile;
 	while (current && (len--))
 	{
-		ft_printf("% 4i", current->val);
+		if (o->color && current == *pile)
+			ft_printf("\033[1;33m% 4i\033[0m", current->val);
+		else if (current == old)
+			ft_printf("\033[1;32m% 4i\033[0m", current->val);
+		else
+			ft_printf("% 4i", current->val);
 		current = current->next;
 	}
 	while (len--)
@@ -29,18 +36,29 @@ void	print_pile(t_list **pile, int len)
 
 void	visu(t_list **a, t_list **b, t_opt *o, char *op)
 {
+	static t_list	*old_a;
+	static t_list	*old_b;
+
+	if (!old_a)
+		old_a = NULL;
+	if (!old_b)
+		old_b = NULL;
 	if (o->visu)
 	{
 		ft_printf("Pile A |");
-		print_pile(a, o->len);
+		print_pile(a, o, NULL);
 		ft_printf("Pile B |");
-		print_pile(b, o->len);
+		print_pile(b, o, NULL);
+		ft_printf("-------------------\n");
 	}
 	else if (o->color && !ft_strcmp(op, "pb\n"))
 	{
 		ft_printf("Pile A |");
-		print_pile(a, o->len);
+		print_pile(a, o, old_a);
 		ft_printf("Pile B |");
-		print_pile(b, o->len);
+		print_pile(b, o, old_b);
+		ft_printf("-------------------\n");
+		old_a = *a ? *a : NULL;
+		old_b = *b ? *b : NULL;
 	}
 }
